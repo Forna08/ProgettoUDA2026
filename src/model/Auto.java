b8de7cde51cdb4e227098b673e21676ab0c62282
bernia <bernia@Q17.pascal.local>
2026-04-29 09:44:15 +0200

    veicolo e auto fatti

--- src/model/Auto.java
+++ src/model/Auto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package model;
 
 import java.time.LocalDate;
 
@@ -45,23 +45,54 @@ public class Auto extends Veicolo{
     }
     
     
-    //setter
+    //setter privv
 
     private void _setPosti(int posti) {
-        
+        if(posti < 2)
+            throw new IllegalArgumentException("numero di posti errato");
         this.posti = posti;
     }
 
     private void _setTipo(String tipo) {
+        if(tipo.equals("") || tipo.isEmpty())
+            throw new IllegalArgumentException("tipologia non corretta");
         this.tipo = tipo;
     }
 
     private void _setvMax(int vMax) {
+        if(vMax <= 0)
+            throw new IllegalArgumentException("vel massima non corretta");
         this.vMax = vMax;
     }
 
     private void _setColore(String colore) {
+        if(colore.equals("") || colore.isEmpty())
+            throw new IllegalArgumentException("colorazione assente");
         this.colore = colore;
     }
+
+    //setter pubbliche
+    public void setPosti(int posti) {
+        this.posti = posti;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setvMax(int vMax) {
+        this.vMax = vMax;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto: " +super.toString() + " posti=" + posti + ", tipo=" + 
+                tipo + ", vMax=" + vMax + ", colore=" + colore;
+    }
+    
     
 }
